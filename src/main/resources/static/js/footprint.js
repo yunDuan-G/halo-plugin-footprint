f1c13239c331b2e0745f8d24b3032d4a2fa37442
cgx0119 <2644656087@qq.com>
2025-05-10 23:58:24 +0800

    优化

--- src/main/resources/static/js/footprint.js
+++ src/main/resources/static/js/footprint.js
@@ -850,8 +850,6 @@ function createInfoWindow(spec) {
                 year: 'numeric',
                 month: '2-digit',
                 day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit'
             }).replace(/\//g, '-');
         } catch {
             return '';
